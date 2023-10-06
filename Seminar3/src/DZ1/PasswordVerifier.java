a027baa2e30d9f27d39a5dba1442792a3d2d9994
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-10-06 16:50:53 +0300

    Seminar2 dz1 fin commit

--- Seminar3/src/DZ1/PasswordVerifier.java
+++ Seminar3/src/DZ1/PasswordVerifier.java
@@ -12,15 +12,47 @@
 
 package DZ1;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PasswordVerifier {
     public static void main(String[] args) {
         String passwd = PasswordReader.Request();
-        boolean result = Verify(passwd);
-        System.out.println(result);
+        boolean result = false;
+        try {
+            result = Verify(passwd);
+        } catch (PassLengthException | PassNumException | PassCapitalException e) {
+            System.out.println("Error: " + e);
+        }
+
+        if (result) {
+            System.out.println("Password " + passwd +"  is correct");
+            System.out.println(passwd);
+        }
     }
 
-    public static boolean Verify(String passwd) {
+    public static boolean Verify(String passwd) throws PassLengthException, PassNumException, PassCapitalException {
+        boolean result = false;
+        // check length
+        if (passwd.length() != 8) {
+            throw new PassLengthException("Password length should get 8 symbols");
+        }
 
-        return  true;
+        // check number
+        Pattern dig_pattern = Pattern.compile("[0-9]");
+        Matcher dig_matcher = dig_pattern.matcher(passwd);
+        result = dig_matcher.find();
+        if (!result) {
+            throw new PassNumException("Password should contain digit");
+        }
+
+        // check Capitalized character
+        Pattern cap_pattern = Pattern.compile("[A-Z]");
+        Matcher cap_matcher = cap_pattern.matcher(passwd);
+        result = cap_matcher.find();
+        if (!result) {
+            throw new PassCapitalException("Password should contain Capitalized character");
+        }
+        return  result;
     }
 }
