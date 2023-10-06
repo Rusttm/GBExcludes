7e8556e99b3209c73a8a488789f9c41bacafdc4f
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-10-06 12:23:48 +0300

    seminar2 DZ1 changes in classes is ready

---
+++ Seminar2/src/DZ1/NumberRequest.java
@@ -0,0 +1,37 @@
+package DZ1;
+
+import java.util.Scanner;
+
+public class NumberRequest {
+    public static void main(String[] args) throws InvalidNumberException {
+        System.out.println(Request());
+    }
+    public static int Request() throws InvalidNumberException {
+        Scanner scan = new Scanner(System.in);
+        int input_num = 0;
+        while (input_num < 1) {
+            try {
+                System.out.println("Enter number");
+                input_num = Integer.parseInt(scan.nextLine());
+                if (input_num < 0) {
+                    throw new NegativeNumberException("Number is negative");
+                }
+                if (input_num == 0) {
+                    throw new ZeroNumberException("Number shouldn't be 0");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Error: unknown number format");
+            } catch (NegativeNumberException e) {
+                System.out.println("Number should be positive");
+                throw new InvalidNumberException("Not correct number");
+            } catch (ZeroNumberException e) {
+                System.out.println("Number should be >0 ");
+                throw new InvalidNumberException("Not correct number");
+            }
+        }
+        scan.close();
+
+        return input_num;
+    }
+
+}
--- Seminar2/src/DZ1/Seminar2Dz1.java
+++ Seminar2/src/DZ1/Seminar2Dz1.java
@@ -11,37 +11,12 @@ import java.util.Scanner;
 public class Seminar2Dz1 {
     public static void main(String[] args) {
         try {
-            System.out.println("Number: " + NumRequest() + " is correct!");
+            int num_req = NumberRequest.Request();
+            System.out.println("Number: " + num_req + " is correct!");
         } catch (InvalidNumberException e) {
             System.out.println("Error: " + e);
         }
 
     }
-    public static int NumRequest() throws InvalidNumberException {
-        Scanner scan = new Scanner(System.in);
-        int age = 0;
-        while (age < 1) {
-            try {
-                System.out.println("Enter number");
-                age = Integer.parseInt(scan.nextLine());
-                if (age < 0) {
-                    throw new NegativeNumberException("Number is negative");
-                }
-                if (age == 0) {
-                    throw new ZeroNumberException("Number shouldn't be 0");
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("Error: unknown number format");
-            } catch (NegativeNumberException e) {
-                System.out.println("Number should be positive");
-                throw new InvalidNumberException("Not correct number");
-            } catch (ZeroNumberException e) {
-                System.out.println("Number should be >0 ");
-                throw new InvalidNumberException("Not correct number");
-            }
-        }
-        scan.close();
 
-        return age;
-    }
 }
